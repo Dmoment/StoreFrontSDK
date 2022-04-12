898675b00a396473a8801ecb70b50d1e89de5aa2
Dmoment <rishudc119@gmail.com>
2022-04-12 21:19:54 +0530

    initial setup

---
+++ lib/vendo/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Vendo
+  VERSION = "0.1.0"
+end
---
+++ spec/vendo_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Vendo do
+  it "has a version number" do
+    expect(Vendo::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
